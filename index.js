f8f133d1f4bf183a204256350df0fdab410adab1
greemaltend4 <dengestabiy@hotmail.com>
2025-11-07 12:41:31 +0000

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const config = require('./config.json');
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Bienvenue sur l intégration des services cloud!');
+});
+
+// Endpoint pour vérifier la connexion aux services
+app.get('/api/services', (req, res) => {
+  res.json(config.connecteurs);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Serveur en cours d exécution sur le port ${PORT}`);
+});
